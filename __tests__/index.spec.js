3e279b99e0573fb0c726d122d2e86c69e61d1bb4
afeiship <1290657123@qq.com>
2020-11-21 11:13:02 +0800

    fix: test case

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,22 +1,8 @@
 (function() {
   const NxTxCosObject = require('../src');
 
-  // var config = require('./config.json');
-  var txObj = new NxTxCosObject(config);
-
-  jest.setTimeout(30 * 1e3);
-
   describe('NxTxCosObject.methods', function () {
     test('dels', function (done) {
-      // txObj
-      //   .empty({
-      //     Bucket: '19967-05-1301823685',
-      //     Region: 'ap-beijing-fsi'
-      //   })
-      //   .then((res) => {
-      //     console.log(res);
-      //     done();
-      //   });
     });
   });
 
